acb8e12fd84ddb62465c1884225b4b46fb89fc19
Katrinerosa <katrinerosa@gmail.com>
2026-01-13 09:22:38 +0100

    Allow remote Pokemon images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // Tillad pokemon-billeder fra GitHub's raw-host.
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/PokeAPI/sprites/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
